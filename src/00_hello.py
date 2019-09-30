0699824f46076dd20ffc26a96f9a133ad54a05f5
kevinsims1 <kevinsims1@outlook.com>
2019-09-30 14:34:07 -0500

    hello and bignum done

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,7 @@
 # Print "Hello, world!" to your terminal
+print("Hello, World!")
+
+
+
+#run `python 00_hello.py` in the terminal
+
--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,7 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+print(f"{2**65536}")
+
+#run python 01_bignum.py
