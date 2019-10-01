ae8baeb203479a43cf1a9a837b747842d729ad80
kevinsims1 <kevinsims1@outlook.com>
2019-10-01 17:18:14 -0500

    8-12 done

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,7 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(x):
+    if(x%2 == 0):
+        print("True")
 
+n = 6
+print(is_even(n))
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +14,11 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def even(c):
+    if c % 2 == 0:
+        print("Even")
+    if c % 2 != 0:
+        print("Odd")
 
+print(even(num))
+# nOTE in my console log i get a recurring None from my last print ask team lead about this 
