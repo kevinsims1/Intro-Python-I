2e71301a72b6d9b9bf863c5dbc76aa48f87f06cb
kevinsims1 <kevinsims1@outlook.com>
2019-10-28 13:38:24 -0500

    mvp

--- src/15_classes.py
+++ src/15_classes.py
@@ -17,6 +17,8 @@ class Waypoint(LatLon):
     def __init__(self, name, lat, lon):
         super().__init__(lat, lon)
         self.name = name
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
 
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
@@ -28,6 +30,8 @@ class Geocache(Waypoint):
         super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}, {self.difficulty}, {self.size}"
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
