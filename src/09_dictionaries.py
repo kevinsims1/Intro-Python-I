982ffa38903d9770a8d85928d46fcbb25df0b971
kevinsims1 <kevinsims1@outlook.com>
2019-12-03 13:20:01 -0600

    mvp

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,8 +35,9 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
-
-
+a = {"lat": 23, "lon": 12, "name": "tuples house"}
+waypoints.append(a.copy())
+print(waypoints)
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
--- src/11_args.py
+++ src/11_args.py
@@ -26,10 +26,10 @@ print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-# a = [7, 6, 5, 4]
+a = [7, 6, 5, 4]
 
 # # What thing do you have to add to make this work?
-# print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 
 
@@ -38,7 +38,10 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(*args):
+    if len(args) == 1:
+        print(*args + 1)
+    print(args[0] + args[1])
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -59,18 +62,18 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
-# Should print
-# key: city, value: Berkeley
-# key: population, value: 121240
-# key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# # Should print
+# # key: city, value: Berkeley
+# # key: population, value: 121240
+# # key: founded, value: "March 23, 1868"
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
-# What thing do you have to add to make this work?
-f4(**d)
+# # What thing do you have to add to make this work?
+# f4(**d)
